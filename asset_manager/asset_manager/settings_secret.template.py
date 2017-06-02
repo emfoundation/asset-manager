5f3e1726da167959cad6b8a96555b9997aa93877
George Millard <george.millard@ellenmacarthurfoundation.org>
2017-06-02 16:29:07 +0100

    Added AWS Settings to settings_secret.template.py

---
+++ asset_manager/asset_manager/settings_secret.template.py
@@ -0,0 +1,24 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+ALLOWED_HOSTS = []
+
+# AWS Settings
+
+AWS_STORAGE_BUCKET_NAME = ''
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
+    }
+}
