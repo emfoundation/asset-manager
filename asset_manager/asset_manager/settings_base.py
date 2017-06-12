1eb0bb4c81e8007c6b10bcb213d378bb9250a3e4
George Millard <george.millard@ellenmacarthurfoundation.org>
2017-06-12 09:54:47 +0100

    REST Api initial commit

--- asset_manager/asset_manager/settings_base.py
+++ asset_manager/asset_manager/settings_base.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- asset_manager/file_manager/views.py
+++ asset_manager/file_manager/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Asset
+from .serializers import AssetSerializer
+
 # Create your views here.
+class AssetViewSet(ModelViewSet):
+    serializer_class = AssetSerializer
+    queryset = Asset.objects.all()
