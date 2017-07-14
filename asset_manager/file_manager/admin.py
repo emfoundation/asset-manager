c9f96163dea60c3c0a3a9080350715bfc8d1aa70
George Millard <george.millard@ellenmacarthurfoundation.org>
2017-07-14 10:35:02 +0100

    Added Asset meta-data fields: last_edit_by, last_edit_at and owner. Added Collection model and ForeignKey field to Asset.

--- asset_manager/file_manager/admin.py
+++ asset_manager/file_manager/admin.py
@@ -7,12 +7,23 @@ from pytz import timezone
 
 class AssetAdmin(admin.ModelAdmin):
     form = forms.AssetForm
-    filter_horizontal = ('contributors', 'locations', 'tags', )
-    list_display = ['name', 'get_path', 'file', 'uploaded_by', 'uploaded_at']
+    filter_horizontal = ('collections', 'contributors', 'locations', 'tags', )
+    list_display = ['name', 'get_path', 'file', 'filetype', 'uploaded_by', 'uploaded_at', 'last_edit_by', 'last_edit_at', 'owner', ]
+
     def save_model(self, request, obj, form, change):
+        # on first save...
         if not change:
             obj.uploaded_by = request.user
             obj.uploaded_at = datetime.now(timezone(settings.TIME_ZONE))
+        # on subsequent saves...
+        else:
+            obj.last_edit_by = request.user
+            obj.last_edit_at = datetime.now(timezone(settings.TIME_ZONE))
+            
+        # owner cannot be empty...
+        if not obj.owner:
+            obj.owner = request.user
+
         super(AssetAdmin, self).save_model(request, obj, form, change)
 
 class FolderAdmin(admin.ModelAdmin):
@@ -25,5 +36,6 @@ admin.site.register(models.Tag)
 admin.site.register(models.Contributor)
 admin.site.register(models.ContinentTagGroup)
 admin.site.register(models.CountryTag)
+admin.site.register(models.Collection)
 admin.site.register(models.Folder, FolderAdmin)
 admin.site.register(models.Asset, AssetAdmin)
