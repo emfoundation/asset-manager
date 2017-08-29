3b546c3437a40e92d2b1f88ce1e1eb054d2a6bec
George Millard <george.millard@ellenmacarthurfoundation.org>
2017-08-29 10:49:20 +0100

    Replaced Add Folder button for now, as it is currently the only way to add Root-level Folders. Added comments to code.

--- asset_manager/file_manager/templatetags/file_manager_tags.py
+++ asset_manager/file_manager/templatetags/file_manager_tags.py
@@ -4,9 +4,13 @@ register = template.Library()
 
 @register.filter
 def show_root_level_folders_only(cl):
-    qs = cl.result_list
-    for folder in qs:
+    """
+    cl is a variable holding a Change List; the list of model instances.
+    This function filters the Change List to remove non-root elements.
+    """
+    query_set = cl.result_list
+    for folder in query_set:
         if folder.parent != None:
-            qs = qs.exclude(pk=folder.id)
-    cl.result_list = qs
+            query_set = query_set.exclude(pk=folder.id)
+    cl.result_list = query_set
     return cl
