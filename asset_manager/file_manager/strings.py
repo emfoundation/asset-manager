2d7080d0e0053f149570cf1d65b1d697be942322
georgemillard <george.millard@ellenmacarthurfoundation.org>
2017-09-19 17:15:03 +0100

    Update strings.py

--- asset_manager/file_manager/strings.py
+++ asset_manager/file_manager/strings.py
@@ -20,7 +20,7 @@ duplicate_model_name_msg = '{0} "{1}" already exists within Folder "{2}" \
 duplicate_inline_model_name = 'You are attempting to add more than one {0} \
 with the same name: "{1}". Please choose unique names for each {0}.'
 
-missing_parent_msg = 'You must select a Parent folder'
+missing_parent_msg = 'No Parent Folder has been selected. Please choose a Parent Folder.'
 
 # string constants
 VALID_NAME_FORMAT = '^[a-zA-Z0-9-_ ]+$'
