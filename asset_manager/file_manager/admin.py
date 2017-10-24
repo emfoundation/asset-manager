eb26385aaf268de5bddac4f21ff7772bbaf9628a
George Millard <georgemillard@Georges-MacBook-Pro.local>
2017-10-24 14:14:26 +0100

    Django Admin site header and title will now show if the environment is development.

--- asset_manager/file_manager/admin.py
+++ asset_manager/file_manager/admin.py
@@ -14,3 +14,6 @@ admin.site.register(models.Tag)
 admin.site.register(models.Folder, FolderAdmin)
 admin.site.register(models.Asset, AssetAdmin)
 
+admin.site.site_header = 'EMF Digital Asset Management System {}'.format('(' + settings.ENVIRONMENT + ')')
+admin.site.site_title = 'EMF DAMS Admin {}'.format('(' + settings.ENVIRONMENT + ')')
+admin.site.index_title = 'Admin Interface'
