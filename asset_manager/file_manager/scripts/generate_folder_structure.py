dcc6b32de6f239f3fc4978a53c94d71e1a764555
Merton Lansley <mertlansley@gmail.com>
2018-02-05 09:43:55 +0000

    Auto create every folder for Content type if it does not exist.

--- asset_manager/file_manager/scripts/generate_folder_structure.py
+++ asset_manager/file_manager/scripts/generate_folder_structure.py
@@ -2,8 +2,7 @@
 from file_manager.models import Asset, Folder
 
 # Changes the Assets parent folder to the specified folder
-def move_asset(asset, folder_name):
-    folder = Folder.objects.get_or_create(name=folder_name)[0]
+def move_asset(asset, folder):
     if asset.parent != folder:
         #print("\n    Moving Asset '%s' into '%s' Folder" % (asset.name, folder_name))
         asset.parent = folder
@@ -16,10 +15,11 @@ def create_structure():
     types_dict = dict(Asset.TYPE_CHOICES)
     for code, type in types_dict.items():
         print("Checking the '%s' Content types..." % type, end='')
+        folder = Folder.objects.get_or_create(name=type)[0]
         assets_by_type = Asset.objects.filter(type_field=code)
         moved = 0
         for asset in assets_by_type:
-            moved += int(move_asset(asset, type))
+            moved += int(move_asset(asset, folder))
 
         print("%s Asset(s) moved " % moved)
 def run():
