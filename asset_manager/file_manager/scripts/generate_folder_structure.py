fd0f9b8610b324af54c10498453eafb9d2c92a5f
Merton Lansley <mertlansley@gmail.com>
2018-02-05 09:36:13 +0000

    Refactored and now skips over assets that don't need moving

--- asset_manager/file_manager/scripts/generate_folder_structure.py
+++ asset_manager/file_manager/scripts/generate_folder_structure.py
@@ -1,22 +1,26 @@
 # Checks the assets currently in use, and sorts them into a folder structure that is structured based on the Content types.
 from file_manager.models import Asset, Folder
-# Moves an Asset into a Folder
+
+# Changes the Assets parent folder to the specified folder
 def move_asset(asset, folder_name):
     folder = Folder.objects.get_or_create(name=folder_name)[0]
     if asset.parent != folder:
-        print("Moving Asset '%s' into '%s' Folder \n" % (asset, type))
+        #print("\n    Moving Asset '%s' into '%s' Folder" % (asset.name, folder_name))
         asset.parent = folder
+        asset.save()
+        return True
 
-    asset.save()
-
+    return False
 
 def create_structure():
     types_dict = dict(Asset.TYPE_CHOICES)
     for code, type in types_dict.items():
-        print("Checking all '%s'|'%s' Assets" % (code, type))
+        print("Checking the '%s' Content types..." % type, end='')
         assets_by_type = Asset.objects.filter(type_field=code)
+        moved = 0
         for asset in assets_by_type:
-            move_asset(asset, type)
+            moved += int(move_asset(asset, type))
 
+        print("%s Asset(s) moved " % moved)
 def run():
     create_structure()
