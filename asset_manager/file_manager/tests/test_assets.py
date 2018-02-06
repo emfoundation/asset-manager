0e0ed54b8d8ce879448d29c4cfc8f83ec3ff16b5
George Millard <georgemillard@Georges-MBP.home>
2018-02-06 12:14:19 +0000

    Fixed a bug whereby moving an Asset with an empty file field would create an erroneous file field entry. Added unit tests.

--- asset_manager/file_manager/tests/test_assets.py
+++ asset_manager/file_manager/tests/test_assets.py
@@ -17,7 +17,195 @@ logging.basicConfig(
 # logging.disable(logging.CRITICAL)
 
 # Create your tests here.
-class AssetModelTests(TestCase):
+
+class AssetModelFileNameTests(TestCase):
+
+    # Create tests for correct Asset filename
+
+    def test_filename_on_create_asset_without_file(self):
+        """
+        Tests filename on create asset without file
+        """
+        logging.info('Test filename on create asset without a file...')
+
+        f = Folder(name='f')
+        f.save()
+
+        a = Asset(name='a', parent=f)
+        a.save()
+
+        self.assertEqual(a.file.name, '')
+
+    # should fail pre #200 fix
+    def test_filename_on_edit_parent_without_file(self):
+        """
+        Test filename on edit parent without file
+        """
+        logging.info('Test filename on edit parent without file...')
+
+        f = Folder(name='f')
+        f.save()
+        f2 = Folder(name='f2')
+        f2.save()
+
+        a = Asset(name='a', parent=f)
+        a.save()
+
+        a.parent=f2
+        a.save()
+
+        self.assertEqual(a.file.name, '')
+
+    # should fail pre #200 fix
+    def test_filename_on_reverse_edit_parent_without_file(self):
+        """
+        Test filename on reverse edit parent without file
+        """
+        logging.info('Test filename on reverse edit parent without file...')
+
+        f = Folder(name='f')
+        f.save()
+        f2 = Folder(name='f2')
+        f2.save()
+
+        a = Asset(name='a', parent=f)
+        a.save()
+
+        a.parent=f2
+        a.save()
+
+        a.parent=f
+        a.save()
+
+        self.assertEqual(a.file.name, '')
+
+    def test_filename_on_add_file(self):
+        """
+        Test filename on add file
+        """
+        logging.info('Test filename on add file...')
+
+        f = Folder(name='f')
+        f.save()
+
+        a = Asset(name='a', parent=f)
+        a.file.save('file.txt', ContentFile('Content'.encode('utf-8')))
+
+        self.assertEqual(a.file.name, str(f.id) + '/file.txt')
+
+    def test_filename_on_edit_parent_and_add_file(self):
+        """
+        Test filename on edit parent and add file
+        """
+        logging.info('Test filename on edit parent and add file...')
+
+        f = Folder(name='f')
+        f.save()
+        f2 = Folder(name='f2')
+        f2.save()
+
+        a = Asset(name='a', parent=f)
+        a.save()
+
+        a.parent = f2
+        a.file.save('file.txt', ContentFile('Content'.encode('utf-8')))
+
+        self.assertEqual(a.file.name, str(f2.id) + '/file.txt')
+
+    def test_filename_on_create_asset_with_file(self):
+        """
+        Tests filename on create asset
+        """
+        logging.info('Test filename on create asset...')
+
+        f = Folder(name='f')
+        f.save()
+
+        a = Asset(name='a', parent=f)
+        a.file.save('file.txt', ContentFile('Content'.encode('utf-8')))
+
+        self.assertEqual(a.file.name, str(f.id) + '/file.txt')
+
+    def test_filename_on_edit_parent_with_file(self):
+        """
+        Test filename on edit parent with file
+        """
+        logging.info('Test filename on edit parent with file...')
+
+        f = Folder(name='f')
+        f.save()
+        f2 = Folder(name='f2')
+        f2.save()
+
+        a = Asset(name='a', parent=f)
+        a.file.save('file.txt', ContentFile('Content'.encode('utf-8')))
+
+        a.parent = f2
+        a.save()
+
+        self.assertEqual(a.file.name, str(f2.id) + '/file.txt')
+
+
+
+    def test_filename_on_reverse_edit_parent_with_file(self):
+        """
+        Test filename on reverse edit parent with file
+        """
+        logging.info('Test filename on reverse edit parent with file...')
+
+        f = Folder(name='f')
+        f.save()
+        f2 = Folder(name='f2')
+        f2.save()
+
+        a = Asset(name='a', parent=f)
+        a.file.save('file.txt', ContentFile('Content'.encode('utf-8')))
+
+        a.parent = f2
+        a.save()
+
+        a.parent = f
+        a.save()
+
+        self.assertEqual(a.file.name, str(f.id) + '/file.txt')
+
+    def test_filename_on_remove_file(self):
+        """
+        Test filename on remove file
+        """
+        logging.info('Test filename on remove file...')
+
+        f = Folder(name='f')
+        f.save()
+
+        a = Asset(name='a', parent=f)
+        a.file.save('file.txt', ContentFile('Content'.encode('utf-8')))
+
+        a.file.delete()
+
+        self.assertEqual(a.file.name, None)
+
+    def test_filename_on_edit_parent_and_remove_file(self):
+        """
+        Test filename on edit parent and remove file
+        """
+        logging.info('Test filename on edit parent and remove file...')
+
+        f = Folder(name='f')
+        f.save()
+        f2 = Folder(name='f2')
+        f2.save()
+
+        a = Asset(name='a', parent=f)
+        a.file.save('file.txt', ContentFile('Content'.encode('utf-8')))
+
+        a.parent = f2
+        a.file.delete()
+
+        self.assertEqual(a.file.name, None)
+
+
+class AssetModelS3Tests(TestCase):
 
     def get_bucket_contents(self):
         contents = s3_utils.s3.list_objects(Bucket = settings.AWS_STORAGE_BUCKET_NAME)
