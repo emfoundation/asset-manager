9fd837291b71c17b8b7cf447715bae741d15c3cb
Merton Lansley <mertlansley@gmail.com>
2018-02-09 12:01:29 +0000

    removed undeeded import and prettified a bit.

--- asset_manager/api/urls.py
+++ asset_manager/api/urls.py
@@ -1,17 +1,28 @@
 from rest_framework.routers import SimpleRouter
 from . import views
 from file_manager import models
-from django.conf.urls import url
 
 router = SimpleRouter()
 
-router.register("assets/collection/(?P<id>\d+)",
-		views.AssetPerCollectionViewSet, 'asset-collection')
-router.register("assets/tag/(?P<id>\d+)", views.AssetPerTagViewSet, 'asset-tag')
-router.register("assets/collection/(?P<collection_id>\d+)/tag/(?P<tag_id>\d+)",
-		views.AssetPerCollectionAndTagViewSet, 'asset-collection-tag')
-router.register("assets/collection/(?P<collection_id>\d+)/location/(?P<location_id>\d+)",
-		views.AssetPerCollectionAndLocationViewSet, 'asset-collection-location')
+router.register(
+	"assets/collection/(?P<id>\d+)",
+	views.AssetPerCollectionViewSet,
+	'asset-collection'
+	)
+router.register(
+	"assets/tag/(?P<id>\d+)",
+	views.AssetPerTagViewSet,
+	'asset-tag'
+	)
+router.register(
+	"assets/collection/(?P<collection_id>\d+)/tag/(?P<tag_id>\d+)",
+	views.AssetPerCollectionAndTagViewSet, 
+	'asset-collection-tag')
+router.register(
+	"assets/collection/(?P<collection_id>\d+)/location/(?P<location_id>\d+)",
+	views.AssetPerCollectionAndLocationViewSet,
+	'asset-collection-location'
+	)
 router.register("assets", views.AssetViewSet,)
 router.register("collections", views.CollectionViewSet)
 router.register("contributors", views.ContributorViewSet)
