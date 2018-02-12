805f7da193e58f216478b83394c70dd878956b69
Merton Lansley <mertlansley@gmail.com>
2018-02-12 13:15:17 +0000

    Filters now hide by default, and change colour when a filter is selected inside of it.

--- asset_manager/file_manager/static/file_manager/js/filter.js
+++ asset_manager/file_manager/static/file_manager/js/filter.js
@@ -6,14 +6,18 @@ $(document).ready(function() {
     $toggleFilter = $('.js-toggle-filter');
     $filters = $('#changelist-filter');
     $choiceList = $filters.find('.choice-list');
-
     $toggleFilter.click(function(evt) {
         toggleChoiceList($(this), $(this).next());
     });
+    $toggleFilter.trigger("click");
 });
 
 function toggleChoiceList($filterLink, $choiceList) {
     isHidden = $choiceList.hasClass('hide');
+    hasSelected = $choiceList
+                    .children()               // Get choices 
+                    .slice(1)                 // Ignores the first choice, which is the 'All' selection
+                    .hasClass('selected');
 
     if (isHidden) {
         $choiceList.removeClass('hide');
@@ -24,6 +28,11 @@ function toggleChoiceList($filterLink, $choiceList) {
         $choiceList.addClass('hide');
         newText = $filterLink.html().replace("Hide","Show");
         $filterLink.html(newText);
+        if (hasSelected) {
+            $filterLink
+                .find('.filter-title')
+                .addClass('selected')
+        }
     }
 }
 
