17aa78c75dc7333709cc2f727346ea37435a2960
Merton Lansley <mertlansley@gmail.com>
2018-02-12 14:06:44 +0000

    Fixed issue causing the filters to not hide on load.

--- asset_manager/file_manager/static/file_manager/js/filter.js
+++ asset_manager/file_manager/static/file_manager/js/filter.js
@@ -6,34 +6,42 @@ $(document).ready(function() {
     $toggleFilter = $('.js-toggle-filter');
     $filters = $('#changelist-filter');
     $choiceList = $filters.find('.choice-list');
+    // Add click events
     $toggleFilter.click(function(evt) {
         toggleChoiceList($(this), $(this).next());
     });
-    $toggleFilter.trigger("click");
+    // Hide filters on load
+    $toggleFilter.each(function(i, filterLink) {        
+        $choiceList = $(filterLink).next();        
+        hideList($(filterLink), $choiceList);
+    });
+        
 });
 
-function toggleChoiceList($filterLink, $choiceList) {
+function toggleChoiceList($filterLink, $choiceList) {    
     isHidden = $choiceList.hasClass('hide');
+    if (isHidden) { showList($filterLink, $choiceList) }
+    else          { hideList($filterLink, $choiceList) }
+}
+
+function showList ($filterLink, $choiceList) {
+    $choiceList.removeClass('hide');
+    newText = $filterLink.html().replace("Show","Hide");
+    $filterLink.html(newText);
+}
+
+function hideList($filterLink, $choiceList) {
     hasSelected = $choiceList
                     .children()               // Get choices 
                     .slice(1)                 // Ignores the first choice, which is the 'All' selection
                     .hasClass('selected');
-
-    if (isHidden) {
-        $choiceList.removeClass('hide');
-        newText = $filterLink.html().replace("Show","Hide");
-        $filterLink.html(newText);
-    }
-    else {
-        $choiceList.addClass('hide');
-        newText = $filterLink.html().replace("Hide","Show");
-        $filterLink.html(newText);
-        if (hasSelected) {
-            $filterLink
-                .find('.filter-title')
-                .addClass('selected')
-        }
+                    
+    $choiceList.addClass('hide');
+    newText = $filterLink.html().replace("Hide","Show");
+    $filterLink.html(newText);
+    if (hasSelected) {
+        $filterLink
+            .find('.filter-title')
+            .addClass('selected')
     }
 }
-
-
