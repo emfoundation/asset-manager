601416db7b563e1a31cda82975d0365a75142eec
Alex Wijns <awijns@gmail.com>
2018-02-19 19:28:09 +0100

    Add prototype version of list and assets per tag group to user_interface

--- asset_manager/user_interface/urls.py
+++ asset_manager/user_interface/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^list$', views.list, name='list'),
+    url(r'^tag/(?P<tag_group_id>\d+)$', views.tag_group, name='tag-group'),
 ]
--- asset_manager/user_interface/views.py
+++ asset_manager/user_interface/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from file_manager.models import TagGroup, Tag, Asset
 
 # Create your views here.
 def index(request):
-     return render(request, 'user_interface/index.html')
+    return render(request, 'user_interface/index.html')
+
+def list(request):
+	tagGroups = TagGroup.objects.all()
+	context = { 'tagGroups': tagGroups }
+	return render(request, 'user_interface/list.html', context)
+
+def tag_group(request, tag_group_id):
+	sub_tags = Tag.objects.filter(group__id=tag_group_id)
+	assets = Asset.objects.filter(tags__in=sub_tags)
+	context = { 'assets': assets }
+	return render(request, 'user_interface/tag.html', context)
