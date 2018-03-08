fe8bab45a06763b27952e7705d6ae8d5a07c24d4
Alex Wijns <awijns@gmail.com>
2018-03-08 16:58:45 +0100

    Add search page

--- asset_manager/user_interface/urls.py
+++ asset_manager/user_interface/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^topic/(?P<topic_id>\d+)$', views.topic, name='topic'),
     url(r'^asset/(?P<asset_id>\d+)$', views.asset, name='asset'),
+    url(r'^search$', views.search, name='search'),
 ]
