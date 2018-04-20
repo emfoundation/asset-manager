aa8ee91380adcc5bee04ae4ac31b445ad0d0b8ed
George Millard <george.millard@ellenmacarthurfoundation.org>
2018-04-20 09:10:18 +0100

    Added Asset per Learner Journey API Route.

--- asset_manager/api/views.py
+++ asset_manager/api/views.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
@@ -27,8 +29,13 @@ class AssetPerLearnerJourneyViewSet(ModelViewSet):
     # Returns all Assets from a given Learner Journey
     def get_queryset(self):
         learner_journey_id = self.kwargs['id']
-        asset_learner_journeys = AssetLearnerJourney.objects.filter(learner_journey=learner_journey_id)
-        print(asset_learner_journeys)        
+        asset_learner_journeys = AssetLearnerJourney.objects.filter(learner_journey=learner_journey_id).order_by('position')
+        asset_query_sets = []
+        for asset_learner_journey in asset_learner_journeys:
+            asset_query_set = Asset.objects.get(id = asset_learner_journey.asset.id)
+            asset_query_sets.append(asset_query_set)
+
+        return list(chain(asset_query_sets))
 
 class AssetPerTagViewSet(ModelViewSet):
     serializer_class = serializers.AssetSerializer
